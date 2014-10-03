7263de2976a09f17bd180d9e7b75b6d12516c938
GaryCurran <garycurran@live.com>
2014-10-03 15:51:21 +0100

    Print

--- Week3/script.sh
+++ Week3/script.sh
@@ -6,6 +6,6 @@
 #: Description : 
 #: Options : 
 
-cpucount = $(grep processor /proc/cpuinfo | wc -l)
+cpucount=$(grep processor /proc/cpuinfo | wc -l)
 
 printf "The number of CPU's is : %s\n" $cpucount
