c5a963737dfcac8195b16cdec7df3e7812993645
your username <your email>
2014-10-10 15:13:23 +0100

    Week 4 Lab 3

---
+++ Week4/data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#: Title : 
+#: Date : 
+#: Author : 
+#: Version :
+#: Description : 
+#: Options : 
+
+echo "Testing Week 4 Lab";
+printf "%s\n" "Hello, World!";
+printf "Hello, %s!\n" "$1"
