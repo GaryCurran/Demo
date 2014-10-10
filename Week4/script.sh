7d7f0ee2576c0f03ee928200b0e69af5d746a003
your username <your email>
2014-10-10 15:51:51 +0100

    User Input File

---
+++ Week4/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#: Title : 
+#: Date : 
+#: Author : 
+#: Version :
+#: Description : 
+#: Options : 
+
+printf "Week 4: User Input %s\n"
+printf "Enter your name and press [ENTER]: "
+read name
+printf "$name enter the month you were born and press [ENTER]: "
+read dob
+printf "$name enter your favourite colour press [ENTER]: "
+read col
+
+printf "Hello $name, Your were born in $dob , and your favourite colour is $col. %s\n"
+
